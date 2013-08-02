97ace77ea1903c797f37677681aac2052d7f9713
Taeo <praryboy@hotmail.com>
2013-08-02 02:26:57 -0600

    Prelim modinfo

---
+++ common/taeo/dsm/lib/Reference.java
@@ -0,0 +1,7 @@
+package taeo.dsm.lib;
+
+public class Reference {
+    public static final String MOD_ID = "DSM";
+    public static final String MOD_NAME = "Deep Sea Mod";
+    public static final String VERSION = "0.0.1";
+}
